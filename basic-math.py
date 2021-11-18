0a3fde4ac70440a1afa30f6c82094f0a7d5d7c13
Justin Jang <78944121+deviju95@users.noreply.github.com>
2021-11-18 17:58:06 +0000

    .

--- basic-math.py
+++ basic-math.py
@@ -12,3 +12,56 @@ while n > num:
   i+=1
 
 print(i)
+
+
+#BOJ 1712 손익분기점
+a,b,c = map(int,input().split())
+
+if b>=c:
+  print(-1)
+else:
+  print(a//(c-b)+1)
+
+
+#BOJ 1193 분수찾기
+input_cnt=int(input())
+max_cnt=1
+line = 1
+
+while input_cnt > max_cnt:
+  line += 1
+  max_cnt += line
+
+steps = max_cnt - input_cnt
+
+if line % 2 == 0:
+  i = line - steps
+  j = 1 + steps
+else:
+  i = 1 + steps
+  j = line - steps
+
+print(f"{i}/{j}")
+
+#BOJ 2869 PUZ
+import math
+a,b,v = map(int,input().split())
+
+day = math.ceil((v-a)/(a-b))+1
+
+print(day)
+
+
+#BOJ 10250 ACM Hotel
+t = int(input())
+
+for _ in range(t):
+  h, w, n = map(int,input().split())
+  floor = n % h
+  room = n // h + 1
+  
+  if n % h == 0:
+    floor = h
+    room = n // h
+     
+  print(floor*100+room)
--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-#BOJ 2292 벌집
-n = int(input())
-num = 1
-i = 1 
-
-while n > num:
-  num += 6 * i
-  i+=1
-
-print(i)
+#BOJ 2775 부녀회장 될테야
