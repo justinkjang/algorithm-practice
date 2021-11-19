cd7003cfeffbfedc20b5eb34e6cb29653a03b148
Justin Jang <78944121+deviju95@users.noreply.github.com>
2021-11-19 18:16:14 +0000

    .

--- basic-math.py
+++ basic-math.py
@@ -65,3 +65,17 @@ for _ in range(t):
     room = n // h
      
   print(floor*100+room)
+
+
+#BOJ 2775 부녀회장 될테야
+t = int(input())
+
+for _ in range(t):
+  k = int(input())
+  n = int(input())
+  f0 = [x for x in range(1, n+1)]
+  for _ in range(k):
+    for i in range(1, n):
+      f0[i] += f0[i-1]
+  
+  print(f0[-1])
