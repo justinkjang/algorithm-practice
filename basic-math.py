68787dca28163ec5cb93f091f15b5da938a8e899
Justin Jang <78944121+deviju95@users.noreply.github.com>
2021-11-21 18:56:05 +0000

    .

--- basic-math.py
+++ basic-math.py
@@ -94,3 +94,24 @@ while sugar >= 0:
   bag += 1
 else:
   print(-1)
+
+
+#BOJ 1011 Fly me to the Alpha Centry
+
+t=int(input())
+for _ in range(t):
+  x,y=map(int,input().split())
+  n = y - x
+
+  count = 0
+  max_D = 0
+  G = 1
+
+  while n > max_D:
+    count += 1
+    max_D += G
+    if count % 2 == 0:
+      G += 1
+
+  print(count)
+
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #BOJ 1011 Fly me to the Alpha Centry
-t = int(input())
 
+t=int(input())
 for _ in range(t):
-    x, y = map(int,input().split())
-    distance = y - x
-    count = 0  # 이동 횟수
-    move = 1  # count별 이동 가능한 거리
-    move_plus = 0  # 이동한 거리의 합
-    while move_plus < distance :
-        count += 1
-        move_plus += move  # count 수에 해당하는 move를 더함
-        if count % 2 == 0 :  # count가 2의 배수일 때, 
-            move += 1  
-    print(count)
+  x,y=map(int,input().split())
+  n = y - x
+
+  count = 0
+  max_D = 0
+  G = 1
+
+  while n > max_D:
+    count += 1
+    max_D += G
+    if count % 2 == 0:
+      G += 1
+
+  print(count)
+
