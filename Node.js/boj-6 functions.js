7905b04e4750b84698c3b0b90b09be59f658bff1
Justin Jang <78944121+deviju95@users.noreply.github.com>
2021-12-26 22:15:38 +0000

    boj-6 functions.js

--- Node.js/boj-6 functions.js
+++ Node.js/boj-6 functions.js
@@ -1,3 +1,30 @@
+// BOJ 4673 Self Numbers
+function d(n) {
+    let sum = n;
+    while (n>0) {
+        sum += n%10
+        n = Math.floor(n/10)
+    }
+    return sum
+}
+function solution(){
+    let selfNum = new Array(100)
+    selfNum.fill(true)
+    let result = '';
+    for (let i = 1; i < 100; i++) {
+        selfNum[d(i)] = false
+    }
+    for (let k = 1; k < 100; k++) {
+        if(selfNum[k]) {
+            result += k + '\n'
+        }
+    }
+    console.log(result)
+}
+solution()
+
+
+
 // BOJ 1065 한수
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 const n = Number(input)
