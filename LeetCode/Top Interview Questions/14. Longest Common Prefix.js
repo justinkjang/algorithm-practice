891cd0e1673f2530c77b002cf916f0c494777ed9
Justin Jang <jangju99@gmail.com>
2022-01-06 12:16:02 -0800

    14. Longest Common Prefix -- updated. error fix

--- LeetCode/Top Interview Questions/14. Longest Common Prefix.js
+++ LeetCode/Top Interview Questions/14. Longest Common Prefix.js
@@ -2,23 +2,14 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    
-    let prefix = ''
-    
-    if (!strs.length) return prefix
-    
-    for (let i = 0; i < strs[0].length; i++){
-        
-        let char = strs[0][i]
-                
-        for( let word = 1; word < strs.length; word++){
-            if (strs[word][i] !== char) {
-                return prefix
-            }
-        }
-        
-        prefix += char
+var longestCommonPrefix = function (strs) {
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let str of strs) {
+      if (str[i] !== strs[0][i]) {
+        return str.slice(0, i);
+      }
     }
-    
+  }
+
+  return strs[0];
 };
