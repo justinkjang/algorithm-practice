a78e93975b2513d1083cfd3cca0001f4a62f2795
Justin Jang <jangju99@gmail.com>
2022-01-10 20:58:53 -0800

    66. Plus One

---
+++ LeetCode/Top Interview Questions/66. Plus One.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+// [9]
+
+var plusOne = function (digits) {
+  // A loooong string of number cannot be changed to integer using Number(), because the number gets rounded.
+};
