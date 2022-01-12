c640dc9c29e9ed1f7b50f55fc69d7d845dadfccd
Justin Jang <jangju99@gmail.com>
2022-01-11 20:40:03 -0800

    88. Merge Sorted Array

---
+++ LeetCode/Top Interview Questions/88. Merge Sorted Array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    
+    let maxIndex = m + n - 1
+    
+    m -= 1
+    n -= 1
+    
+    while (n >= 0) {
+        if (nums1[m] > nums2[n]) {
+            nums1[maxIndex] = nums1[m]
+            m -= 1
+            maxIndex -= 1
+        } else {
+            nums1[maxIndex] = nums2[n]
+            n -= 1
+            maxIndex -= 1
+        }
+    }
+};
