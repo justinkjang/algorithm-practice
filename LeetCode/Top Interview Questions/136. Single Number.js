360e570849fbe667516443fe458bd22328095c95
Justin Jang <jangju99@gmail.com>
2022-01-14 21:32:33 -0800

    136. Single Number

---
+++ LeetCode/Top Interview Questions/136. Single Number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  const hash = {};
+
+  for (let num of nums) {
+    hash[num] = hash[num] + 1 || 1;
+  }
+
+  for (let key in hash) {
+    if (hash[key] === 1) {
+      return key;
+    }
+  }
+};
