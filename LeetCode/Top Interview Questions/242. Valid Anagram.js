22efded4293a7223021ff76d5cd11ccce92b5a76
Justin Jang <jangju99@gmail.com>
2022-01-23 10:02:54 -0800

    242. Valid Anagram

--- LeetCode/Top Interview Questions/242. Valid Anagram.js
+++ LeetCode/Top Interview Questions/242. Valid Anagram.js
@@ -4,19 +4,23 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
   let ht = {};
 
-  for (let i = 0; i < s.length; i++) {
-    ht[s[i]] = ht[s[i]] + 1 || 1;
+  for (let letter of s) {
+    ht[letter] = ht[letter] + 1 || 1;
   }
 
-  for (let j = 0; j < t.length; j++) {
-    ht[t[j]] = ht[t[j]] - 1 || 0;
+  for (let letter of t) {
+    if (ht[letter] === undefined) {
+      return false;
+    }
+    if (ht[letter] < 1) {
+      return false;
+    }
+    ht[letter]--;
   }
 
-  console.log(ht);
-  // for (key in ht) {
-  //     if(ht[key] > 0) return false
-  // }
-  // return true
+  return true;
 };
