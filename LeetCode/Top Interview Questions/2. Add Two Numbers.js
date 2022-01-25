aa8d0ea26a4121be7cded21002e766fa4b500ff5
Justin Jang <jangju99@gmail.com>
2022-01-25 11:58:35 -0800

    2. Add Two Numbers update with BigO

--- LeetCode/Top Interview Questions/2. Add Two Numbers.js
+++ LeetCode/Top Interview Questions/2. Add Two Numbers.js
@@ -10,38 +10,28 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    let head = new ListNode(0)
-    let carry = 0
-    let node = head
-    
-    while (l1 || l2) {
-        let l1Val = l1 ? l1.val : 0
-        let l2Val = l2 ? l2.val : 0
-        
-        let sum = l1Val + l2Val + carry
-        carry = 0
-        let newVal = sum
-        
-        if(sum > 9) {
-            newVal = sum % 10
-            carry = 1
-        }
-        
-        if(l1){
-            l1 = l1.next
-        }
-        if(l2){
-            l2 = l2.next
-        }
-        
-        node.next = new ListNode(newVal)
-        node = node.next
-    }
-    
-    if(carry){
-        node.next = new ListNode(carry)
-    }
-    
-    return head.next
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0;
+  let head = new ListNode(-1);
+  let node = head;
+
+  while (l1 || l2 || carry) {
+    // if .val is null, I need to make it 0
+    let l1Val = l1 ? l1.val : 0;
+    let l2Val = l2 ? l2.val : 0;
+
+    let nextDigit = (l1Val + l2Val + carry) % 10;
+    node.next = new ListNode(nextDigit);
+    node = node.next;
+
+    carry = Math.floor((l1Val + l2Val + carry) / 10);
+
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+
+  return head.next;
+
+  // TimeC = O(n) :: go through each list once. 2N.
+  // SpaceC = O(n) :: create new ListNode relative to linear input. N.
 };
