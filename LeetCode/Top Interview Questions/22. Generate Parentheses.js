f5e16b163179f37b4407a4e52d573104c3825730
Justin Jang <jangju99@gmail.com>
2022-01-29 21:38:20 -0800

    22. Generate Parentheses

---
+++ LeetCode/Top Interview Questions/22. Generate Parentheses.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  // global result
+  let result = [];
+
+  // dfs recursion helper function && Back Tracking method
+  const dfs = (i, n, slate, openCount, closeCount) => {
+    // base case && backtracking case
+    if (openCount > n) return;
+    if (closeCount > openCount) return;
+    if (i === n * 2) {
+      result.push(slate.join(''));
+      return;
+    }
+
+    // recursion case
+
+    // adding open parentheses case
+    slate.push('(');
+    dfs(i + 1, n, slate, openCount + 1, closeCount); // (1, n, ['('], 1, 0)
+    slate.pop();
+
+    // adding close parentheses case
+    slate.push(')');
+    dfs(i + 1, n, slate, openCount, closeCount + 1);
+    slate.pop();
+  };
+
+  dfs(0, n, [], 0, 0);
+
+  return result;
+};
+
+//Search about time and space complexity. Can't understand
+//T:: O(2^n*2 *n)
+//S:: O(2^n *N)
