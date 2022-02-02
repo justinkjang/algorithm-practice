e064a7320fc62a2e62e0201be3d8381528ccccd0
Justin Jang <jangju99@gmail.com>
2022-02-01 22:55:19 -0800

    287. Find the Duplicate Number

--- LeetCode/Top Interview Questions/287. Find the Duplicate Number.js
+++ LeetCode/Top Interview Questions/287. Find the Duplicate Number.js
@@ -2,4 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function (nums) {};
+var findDuplicate = function (nums) {
+  // use that array only contain nums in the range of indices
+
+  let fast = 0;
+  let slow = 0;
+
+  while (true) {
+    fast = nums[nums[fast]];
+    slow = nums[slow];
+
+    if (fast === slow) {
+      let pointer = 0;
+
+      while (pointer !== slow) {
+        pointer = nums[pointer];
+        slow = nums[slow];
+      }
+
+      return pointer;
+    }
+  }
+};
+// t = n, s= 1
