2407af6a5cf3d59e199a40e048ce49cb402bb95f
Justin Jang <jangju99@gmail.com>
2022-02-02 13:00:58 -0800

    1. Two Sum

--- LeetCode/Top Interview Questions/1. Two Sum.js
+++ LeetCode/Top Interview Questions/1. Two Sum.js
@@ -1,41 +1,62 @@
 // 1. Two Sum
+// andy gala
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    hash[nums[i]] = i;
+  }
 
-    // brute force
-var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [i, j]
-            }
-        }
+  for (let i = 0; i < nums.length; i++) {
+    let potentialKey = target - nums[i];
+
+    if (hash[potentialKey] && hash[potentialKey] !== i) {
+      return [i, hash[potentialKey]];
     }
+  }
 };
-    // Two-pass Hash Table (JS object)
-var twoSum = function(nums, target) {
-    let indices = {};
-    
-    nums.forEach((item, index)=> {
-        indices[item] = index
-    })
-    
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-        if (indices[complement] != undefined && indices[complement] != i) {
-            return [i, indices[complement]]
-        }
+//T = n, s = n
+
+// brute force
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
     }
+  }
 };
-    // One-pass Hash Table
-var twoSum = function(nums, target) {
-    const indices = new Map();
+// Two-pass Hash Table (JS object)
+var twoSum = function (nums, target) {
+  let indices = {};
 
-    for (let index = 0; index < nums.length; index++) {
-        const complement = target - nums[index];
+  nums.forEach((item, index) => {
+    indices[item] = index;
+  });
 
-        if (indices.has(complement)) {
-            return [indices.get(complement), index]
-        }
-
-        indices.set(nums[index], index)
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (indices[complement] != undefined && indices[complement] != i) {
+      return [i, indices[complement]];
     }
+  }
+};
+// One-pass Hash Table
+var twoSum = function (nums, target) {
+  const indices = new Map();
+
+  for (let index = 0; index < nums.length; index++) {
+    const complement = target - nums[index];
+
+    if (indices.has(complement)) {
+      return [indices.get(complement), index];
+    }
+
+    indices.set(nums[index], index);
+  }
 };
