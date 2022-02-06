01656e6a5dec8a51184b1f8c4395946c6f30aced
Justin Jang <jangju99@gmail.com>
2022-02-06 15:57:42 -0800

    238. Product of Array Except Self

--- LeetCode/Top Interview Questions/238. Product of Array Except Self.js
+++ LeetCode/Top Interview Questions/238. Product of Array Except Self.js
@@ -3,5 +3,25 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  // still getting it
+  let left = new Array(nums.length).fill(0);
+  let right = new Array(nums.length).fill(0);
+
+  left[0] = 1;
+  right[right.length - 1] = 1;
+
+  for (let i = 1; i < left.length; i++) {
+    left[i] = left[i - 1] * nums[i - 1];
+  }
+  for (let i = right.length - 2; i >= 0; i--) {
+    right[i] = right[i + 1] * nums[i + 1];
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = left[i] * right[i];
+  }
+
+  return nums;
 };
+
+//t= n/ s=n
+// special optimized way == creating left and right array
