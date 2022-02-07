79f818236986d4770f3184e25db0cc7a43ea25af
Justin Jang <jangju99@gmail.com>
2022-02-06 16:13:53 -0800

    977. Squares of a Sorted Array

---
+++ LeetCode/Top Interview Questions/977. Squares of a Sorted Array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  let result = new Array(nums.length).fill(0);
+  let left = 0;
+  let right = nums.length - 1;
+  let resultIdx = nums.length - 1;
+
+  while (left <= right) {
+    let leftVal = Math.pow(nums[left], 2);
+    let rightVal = Math.pow(nums[right], 2);
+
+    if (leftVal < rightVal) {
+      result[resultIdx] = rightVal;
+      right--;
+    } else {
+      result[resultIdx] = leftVal;
+      left++;
+    }
+
+    resultIdx--;
+  }
+
+  return result;
+};
+
+// optimized way: using two pointer system. Because, nums is sorted already.
+// t = n s = n
