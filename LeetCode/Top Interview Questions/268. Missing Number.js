8641b1918555c9da1088706b5bcd4cb20386d81b
Justin Jang <jangju99@gmail.com>
2022-02-10 19:52:53 -0800

    268. Missing Number

--- LeetCode/Top Interview Questions/268. Missing Number.js
+++ LeetCode/Top Interview Questions/268. Missing Number.js
@@ -1,3 +1,18 @@
+// 02102022
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  let gSum = (nums.length * (nums.length + 1)) / 2;
+  let nSum = nums.reduce((acc, el) => acc + el, 0);
+
+  return gSum - nSum;
+};
+
+// optimize:: use sum of all algorithm.
+// t = n, s = 1
+
 /**
  * @param {number[]} nums
  * @return {number}
