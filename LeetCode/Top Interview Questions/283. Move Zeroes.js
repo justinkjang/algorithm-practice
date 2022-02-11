41fc15b47d26e839c20141d4459177d7f03f6b8f
Justin Jang <jangju99@gmail.com>
2022-02-10 19:42:34 -0800

    283. Move Zeroes

--- LeetCode/Top Interview Questions/283. Move Zeroes.js
+++ LeetCode/Top Interview Questions/283. Move Zeroes.js
@@ -1,3 +1,25 @@
+// 02102022
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let tracker = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[tracker] = nums[i];
+      tracker += 1;
+    }
+  }
+
+  for (let i = tracker; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+};
+
+// t = n, s = 1
+
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
