71550c92f58919c57d8e7e1981b39ecd4b9159a8
Justin Jang <jangju99@gmail.com>
2022-02-20 14:46:58 -0800

    88

--- LeetCode/Top Interview Questions/88. Merge Sorted Array.js
+++ LeetCode/Top Interview Questions/88. Merge Sorted Array.js
@@ -1,3 +1,34 @@
+//02202022
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
+  let i = nums1.length - 1;
+
+  while (second >= 0) {
+    let fVal = nums1[first];
+    let sVal = nums2[second];
+
+    if (fVal > sVal) {
+      nums1[i] = fVal;
+      i--;
+      first--;
+    } else {
+      nums1[i] = sVal;
+      i--;
+      second--;
+    }
+  }
+};
+
+// Optimize:: Two Pointers      Brute Force:: HashMap
+// T = n        S = 1
 // 02122022
 /**
  * @param {number[]} nums1
