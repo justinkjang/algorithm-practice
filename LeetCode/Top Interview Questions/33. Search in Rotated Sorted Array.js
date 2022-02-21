4115322f0513a483742614c90a416ccc873d1d02
Justin Jang <jangju99@gmail.com>
2022-02-20 17:42:55 -0800

    33. Search in Rotated Sorted Array

---
+++ LeetCode/Top Interview Questions/33. Search in Rotated Sorted Array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    // moving mid pointer to target is key.
+    if (nums[mid] === target) return mid;
+
+    if (nums[left] <= nums[mid]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target <= nums[right] && target > nums[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
+
+//HINT! Sorted array => use binary search. two pointer. log n
