0e6c4f9bf09d5fc31d013908dcedb7bba7cecfb9
Justin Jang <jangju99@gmail.com>
2022-02-23 17:35:44 -0800

    74

--- LeetCode/Top Interview Questions/74. Search a 2D Matrix.js
+++ LeetCode/Top Interview Questions/74. Search a 2D Matrix.js
@@ -1,3 +1,34 @@
+// 02 23 2022
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  let row = matrix.length;
+  let col = matrix[0].length;
+  let left = 0;
+  let right = row * col - 1;
+
+  // binary search
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+    let midVal = matrix[Math.floor(mid / col)][mid % col];
+
+    if (midVal === target) return true;
+
+    if (target > midVal) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+};
+// HINT! sorted matrix: binary search ==> even further, the whole matrix is sorted.
+// t= log n, s=1
+
 /**
  * @param {number[][]} matrix
  * @param {number} target
