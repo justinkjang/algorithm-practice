3fbb6e1163666f228ca4fb8b3afbca720ae742fd
Justin Jang <jangju99@gmail.com>
2022-02-26 14:02:02 -0800

    103

---
+++ LeetCode/Top Interview Questions/103. Binary Tree Zigzag Level Order Traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  if (!root) return [];
+
+  const queue = [root];
+  const result = [];
+  let count = 0;
+
+  while (queue.length) {
+    let len = queue.length;
+    if (count % 2 === 0) result.push(queue.map((node) => node.val));
+    else result.push(queue.map((node) => node.val).reverse());
+    count++;
+
+    while (len--) {
+      let node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  return result;
+};
+// HINT! Binary Tree BFS template
+// T = n, s = n
