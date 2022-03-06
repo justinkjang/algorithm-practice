b4f8f72bd82310b7089977d6aa6dd2454725f7db
Justin Jang <jangju99@gmail.com>
2022-03-06 00:01:44 -0800

    108

--- LeetCode/Top Interview Questions/108. Convert Sorted Array to Binary Search Tree.js
+++ LeetCode/Top Interview Questions/108. Convert Sorted Array to Binary Search Tree.js
@@ -1,3 +1,33 @@
+// 03 06 2022
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums, left = 0, right = nums.length - 1) {
+  // base case
+  if (left > right) return null;
+
+  // create node
+  let mid = Math.floor(left + (right - left) / 2);
+  let root = new TreeNode(nums[mid]);
+
+  // recursive case
+  root.left = sortedArrayToBST(nums, left, mid - 1);
+  root.right = sortedArrayToBST(nums, mid + 1, right);
+
+  return root;
+};
+// HINT! Recursive BST with 3 pointers
+// T = N, S = N
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
