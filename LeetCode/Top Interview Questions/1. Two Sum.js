94cc16ab92d90de7abc1ac5d0a44b0170bd50791
Justin Jang <jangju99@gmail.com>
2022-03-08 10:47:54 -0800

    1

--- LeetCode/Top Interview Questions/1. Two Sum.js
+++ LeetCode/Top Interview Questions/1. Two Sum.js
@@ -1,3 +1,27 @@
+// 03 08 2022
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    hash[nums[i]] = i;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let potKey = target - nums[i];
+
+    if (hash[potKey] && hash[potKey] !== i) {
+      return [i, hash[potKey]];
+    }
+  }
+};
+//HINT! brute = n^2. instead use HASH & target - nums[i]
+//T = n, s = n
+
 // 1. Two Sum
 // andy gala
 /**
