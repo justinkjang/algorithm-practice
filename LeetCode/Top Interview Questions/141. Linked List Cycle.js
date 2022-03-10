ca1cb9ab84cc423ee706501f259f17ce89cd021d
Justin Jang <jangju99@gmail.com>
2022-03-10 01:02:57 -0800

    141

--- LeetCode/Top Interview Questions/141. Linked List Cycle.js
+++ LeetCode/Top Interview Questions/141. Linked List Cycle.js
@@ -1,3 +1,32 @@
+// 03 10 2022
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) return true;
+  }
+
+  return false;
+};
+// HINT! cycle = fast and slow pointer
+//T = linear, S = constant
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
