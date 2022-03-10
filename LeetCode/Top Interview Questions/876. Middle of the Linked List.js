da4ba150d61c2f79b4fcdc2b3abefce60810eeab
Justin Jang <jangju99@gmail.com>
2022-03-10 00:46:29 -0800

    876

--- LeetCode/Top Interview Questions/876. Middle of the Linked List.js
+++ LeetCode/Top Interview Questions/876. Middle of the Linked List.js
@@ -1,3 +1,35 @@
+// 03 10 2022
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var middleNode = function(head) {
+    
+  // error case -> node always exist
+  
+  // two pointers
+  let fast = head
+  let slow = head
+  
+  while(fast && fast.next){
+      fast = fast.next.next
+      slow = slow.next
+  }
+  
+  return slow
+  
+};
+// HINT! Linked List, mid node ==> Fast&Slow method
+// T = linear, S = constant
+
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
