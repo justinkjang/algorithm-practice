145d0885b289b7aca0ed4bd47deae214b89c81e4
Justin Kyumin Jang <jangju99@gmail.com>
2022-04-21 20:03:23 +0900

    Solve 141

--- LeetCode/141. Linked List Cycle.js
+++ LeetCode/141. Linked List Cycle.js
@@ -1,3 +1,35 @@
+// 04 21 2022
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+};
+// Used:: fast and slow method
+// Time:: n
+// Space:: 1
+
 // 03 10 2022
 /**
  * Definition for singly-linked list.
