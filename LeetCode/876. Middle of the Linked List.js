5ee2137464896ddbad9e2e01063d1dd35fcd08ce
Justin Kyumin Jang <jangju99@gmail.com>
2022-04-21 19:49:13 +0900

    Update folder structure

--- LeetCode/876. Middle of the Linked List.js
+++ LeetCode/876. Middle of the Linked List.js
@@ -1,3 +1,30 @@
+// 04 21 2022
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  return slow;
+};
+// Used:: Fast Slow Pointers
+// Time:: n
+// Space:: 1
+
 // 03 10 2022
 /**
  * Definition for singly-linked list.
@@ -10,25 +37,22 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var middleNode = function(head) {
-    
+var middleNode = function (head) {
   // error case -> node always exist
-  
+
   // two pointers
-  let fast = head
-  let slow = head
-  
-  while(fast && fast.next){
-      fast = fast.next.next
-      slow = slow.next
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
   }
-  
-  return slow
-  
+
+  return slow;
 };
 // HINT! Linked List, mid node ==> Fast&Slow method
 // T = linear, S = constant
-
 
 /**
  * Definition for singly-linked list.
