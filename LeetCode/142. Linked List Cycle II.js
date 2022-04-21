c9ecfdcffca8e9432ed58f8c7549156c47d10cfb
Justin Kyumin Jang <jangju99@gmail.com>
2022-04-21 20:31:50 +0900

    Solved 142

--- LeetCode/142. Linked List Cycle II.js
+++ LeetCode/142. Linked List Cycle II.js
@@ -1,3 +1,40 @@
+// 04 21 2022
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  let fast = head;
+  let slow = head;
+  let pointer = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      while (pointer !== slow) {
+        pointer = pointer.next;
+        slow = slow.next;
+      }
+      return pointer;
+    }
+  }
+
+  return null;
+};
+// Used:: Fast and Slow with third Pointer method
+// Time:: n
+// Space:: 1
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
