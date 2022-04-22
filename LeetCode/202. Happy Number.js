b4c1052c8a041fc2d1e4860583d08b82832244b5
Justin Kyumin Jang <jangju99@gmail.com>
2022-04-22 11:50:49 +0900

    Solve 202

--- LeetCode/202. Happy Number.js
+++ LeetCode/202. Happy Number.js
@@ -1,3 +1,37 @@
+// 04 22 2022
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var getNum = function (x) {
+  let result = 0;
+
+  while (x > 0) {
+    result += Math.pow(x % 10, 2);
+    x = Math.floor(x / 10);
+  }
+
+  return result;
+};
+
+var isHappy = function (n) {
+  let fast = n;
+  let slow = n;
+
+  while (true) {
+    fast = getNum(getNum(fast));
+    slow = getNum(slow);
+
+    if (fast === slow) {
+      return fast === 1;
+    }
+  }
+};
+// Used::   Floyd's Cycle Method
+// Time::   log n
+// Space::  1
+
 /**
  * @param {number} n
  * @return {boolean}
