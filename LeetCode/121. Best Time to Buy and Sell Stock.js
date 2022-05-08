6de2eb2ea825f4e56cf01b12728d3d53223459e2
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-08 09:32:55 +0900

    Solve 121

--- LeetCode/121. Best Time to Buy and Sell Stock.js
+++ LeetCode/121. Best Time to Buy and Sell Stock.js
@@ -1,3 +1,23 @@
+// 2022 05 08
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let max = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    min = Math.min(prices[i], min);
+
+    max = Math.max(prices[i] - min, max);
+  }
+
+  return max;
+};
+// Time:: n
+// Space:: 1
+
 /**
  * @param {number[]} prices
  * @return {number}
