f54fda91a910d2da22d20b263667c6ef63cd1837
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-14 09:49:22 +0900

    Recap 896 arrays

--- LeetCode/896. Monotonic Array.js
+++ LeetCode/896. Monotonic Array.js
@@ -1,3 +1,32 @@
+// 05 14 2022
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function (nums) {
+  const same = nums[0] === nums[nums.length - 1];
+  const increasing = nums[0] < nums[nums.length - 1];
+  const decreasing = nums[0] > nums[nums.length - 1];
+
+  if (same) {
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i - 1] !== nums[i]) return false;
+    }
+  } else if (increasing) {
+    for (let i = 1; i < nums.length; i++) {
+      // num[i] is smaller or eq then i-1
+      if (nums[i - 1] > nums[i]) return false;
+    }
+  } else if (decreasing) {
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i - 1] < nums[i]) return false;
+    }
+  }
+  return true;
+};
+// Time: n
+// Space: 1
+
 /**
  * @param {number[]} nums
  * @return {boolean}
