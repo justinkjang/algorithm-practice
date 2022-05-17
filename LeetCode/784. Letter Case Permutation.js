773137383ec607db5a0d1c32655d71ccc93e5c60
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-17 12:17:08 +0900

    Recap 784 recursion

--- LeetCode/784. Letter Case Permutation.js
+++ LeetCode/784. Letter Case Permutation.js
@@ -1,3 +1,45 @@
+// 05 17 2022
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var letterCasePermutation = function (s) {
+  // combination, permutation, subsets === DFS backtracking
+  const result = [];
+
+  const dfs = (i, s, slate) => {
+    if (i === s.length) {
+      result.push(slate.join(''));
+      return;
+    }
+
+    let char = s[i];
+
+    // if is not integer
+    if (!Number.isInteger(parseInt(char))) {
+      // upper case
+      slate.push(char.toUpperCase());
+      dfs(i + 1, s, slate);
+      slate.pop();
+
+      // lower case
+      slate.push(char.toLowerCase());
+      dfs(i + 1, s, slate);
+      slate.pop();
+    } else {
+      slate.push(char);
+      dfs(i + 1, s, slate);
+      slate.pop();
+    }
+  };
+
+  dfs(0, s, []);
+
+  return result;
+};
+// Time: 2^n * n
+// Space: 2^n * n
+
 /**
  * @param {string} s
  * @return {string[]}
