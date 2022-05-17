f1b07b44c486b8b95b487674f7374d1779b559f2
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-17 13:24:50 +0900

    Recap 78 recursion

--- LeetCode/78. Subsets.js
+++ LeetCode/78. Subsets.js
@@ -1,3 +1,33 @@
+// 05 17 2022
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  const result = [];
+
+  const dfs = (i, nums, slate) => {
+    if (i === nums.length) {
+      result.push(slate.slice());
+      return;
+    }
+
+    let el = nums[i];
+
+    dfs(i + 1, nums, slate);
+
+    slate.push(el);
+    dfs(i + 1, nums, slate);
+    slate.pop();
+  };
+
+  dfs(0, nums, []);
+
+  return result;
+};
+// Time: 2^n * n == recursion logic && result scan
+// Space: 2^n * n == result && recursion stack
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
