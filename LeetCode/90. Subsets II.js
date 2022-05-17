1fd7ab02cb11c5435374281142302689741eec37
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-17 13:43:48 +0900

    Recap 90 recursion

--- LeetCode/90. Subsets II.js
+++ LeetCode/90. Subsets II.js
@@ -1,3 +1,39 @@
+// 05 17 2022
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  const result = [];
+  nums.sort((a, b) => a - b);
+
+  const dfs = (i, nums, slate) => {
+    if (i === nums.length) {
+      result.push(slate.slice());
+      return;
+    }
+
+    let el = nums[i];
+
+    dfs(i + 1, nums, slate);
+
+    slate.push(el);
+    dfs(i + 1, nums, slate);
+    slate.pop();
+  };
+
+  dfs(0, nums, []);
+
+  // filter out duplicates
+  let hash = {};
+  for (let el of result) {
+    if (hash[el]) continue;
+    hash[el] = el;
+  }
+
+  return Object.values(hash);
+};
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
