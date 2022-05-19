24c6c72b0749ec3af13efa3ce4e55de8fde61dfa
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-19 14:32:41 +0900

    Recap 216 combination dfs

--- LeetCode/216. Combination Sum III.js
+++ LeetCode/216. Combination Sum III.js
@@ -1,3 +1,42 @@
+// 05 19 2022
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function (k, n) {
+  const result = [];
+
+  // k = depth
+
+  const nums = []; // [1, 2, 3, 4, ... 9]
+  for (let i = 1; i < 10; i++) {
+    nums.push(i);
+  }
+
+  const dfs = (i, k, n, nums, slate) => {
+    // backtrack: stop if more than n
+    if (n < 0) return;
+    // base: stop when reached k
+    if (slate.length === k) {
+      if (n === 0) {
+        result.push(slate.slice());
+      }
+      return;
+    }
+    // dfs
+    for (let j = i; j < nums.length; j++) {
+      slate.push(nums[j]);
+      dfs(j + 1, k, n - nums[j], nums, slate);
+      slate.pop();
+    }
+  };
+
+  dfs(0, k, n, nums, []);
+
+  return result;
+};
+
 /**
  * @param {number} k
  * @param {number} n
