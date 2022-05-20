7b0bf83a94c9d50a75f162ebb817e356d993c389
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-20 12:01:22 +0900

    Recap 90 subsets dfs

--- LeetCode/90. Subsets II.js
+++ LeetCode/90. Subsets II.js
@@ -1,3 +1,37 @@
+// 05 20 2022
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  const result = [];
+
+  const dfs = (i, nums, slate) => {
+    if (i === nums.length) {
+      result.push(slate.slice());
+      return;
+    }
+
+    dfs(i + 1, nums, slate);
+
+    slate.push(nums[i]);
+    dfs(i + 1, nums, slate);
+    slate.pop();
+  };
+
+  dfs(0, nums, []);
+
+  let hash = {};
+  for (let el of result) {
+    if (hash[el]) continue;
+    hash[el] = el;
+  }
+
+  return Object.values(hash);
+};
+
 // 05 18 2022
 /**
  * @param {number[]} nums
