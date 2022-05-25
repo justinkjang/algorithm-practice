c00e59df5e4ca78c5ea1498aa7f1bf2d306b251d
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-25 17:48:29 +0900

    Solve 509 dfs

---
+++ LeetCode/509. Fibonacci Number.js
@@ -0,0 +1,12 @@
+// 05 25 2022
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  if (n === 1) return 1;
+  if (n === 0) return 0;
+
+  return fib(n - 1) + fib(n - 2);
+};
+// using dfs
