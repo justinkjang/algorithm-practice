031802018801b3307d3735c40ead19f3675a0d31
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-31 19:09:07 +0900

    Recap 17 combination dfs

--- LeetCode/17. Letter Combinations of a Phone Number.js
+++ LeetCode/17. Letter Combinations of a Phone Number.js
@@ -1,3 +1,45 @@
+// 05 31 2022
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+  if (digits.length === 0) return [];
+
+  const result = [];
+
+  const hash = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
+
+  const dfs = (i, digits, slate) => {
+    //base
+    if (i === digits.length) {
+      result.push(slate.join(''));
+      return;
+    }
+
+    let dial = hash[digits[i]];
+    //dfs
+    for (let j = 0; j < dial.length; j++) {
+      slate.push(dial[j]);
+      dfs(i + 1, digits, slate);
+      slate.pop();
+    }
+  };
+
+  dfs(0, digits, []);
+  return result;
+};
+// 4^n * n time
+
 // 05 25 2022
 /**
  * @param {string} digits
