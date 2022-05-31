41b0e2d4dd0abc41b32e2b59a547ee75e671c574
Justin Kyumin Jang <jangju99@gmail.com>
2022-05-31 14:55:00 +0900

    Recap 47 permutation dfs

--- LeetCode/47. Permutations II.js
+++ LeetCode/47. Permutations II.js
@@ -1,3 +1,34 @@
+//05 31 2022
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function (nums) {
+  const result = [];
+
+  const dfs = (i, nums) => {
+    // base
+    if (i === nums.length) {
+      result.push(nums.slice());
+      return;
+    }
+
+    // dfs
+    let hash = {};
+    for (let j = i; j < nums.length; j++) {
+      if (hash[nums[j]]) continue;
+      hash[nums[j]] = true;
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      dfs(i + 1, nums);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  };
+
+  dfs(0, nums);
+
+  return result;
+};
+
 // 05 20 2022
 /**
  * @param {number[]} nums
