a3412882e5ed69c5f791dace82058bd8c0a1e5e4
Justin Kyumin Jang <jangju99@gmail.com>
2022-06-24 12:40:20 -0700

    Recap 643 sliding window

--- LeetCode/643. Maximum Average Subarray I.js
+++ LeetCode/643. Maximum Average Subarray I.js
@@ -1,3 +1,36 @@
+//06 24 2022
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  // SLIDING WINDOW
+  // keywords:: contiguous, subarray
+
+  // Bruteforce:: t:n^2 s:n
+  // Sliding Window:: t:n s:1
+
+  //max variable, start, end, soFar
+  let max = -Infinity;
+  let windowStart = 0;
+  let soFar = 0;
+
+  for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+    soFar += nums[windowEnd];
+
+    if (windowEnd - windowStart === k - 1) {
+      let avg = soFar / k;
+      max = Math.max(max, avg);
+
+      soFar -= nums[windowStart];
+      windowStart++;
+    }
+  }
+
+  return max;
+};
+
 /**
  * @param {number[]} nums
  * @param {number} k
