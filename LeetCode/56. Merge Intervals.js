0467f7c8bca57ac466d73a7a99267c550d9f477e
Justin Kyumin Jang <jangju99@gmail.com>
2022-06-25 22:04:32 -0700

    Recap 56 arrays

--- LeetCode/56. Merge Intervals.js
+++ LeetCode/56. Merge Intervals.js
@@ -1,3 +1,35 @@
+// 06 25 2022
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  // sort by first number
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  // start with first sub array
+  let result = [intervals[0]]; // [[1,3]]
+
+  for (let interval of intervals) {
+    // [2,6]
+    let e1 = result[result.length - 1][1]; // 3
+    let s2 = interval[0]; // 2
+    let e2 = interval[1]; // 6
+
+    if (e1 >= s2) {
+      // 3 >= 2
+      result[result.length - 1][1] = Math.max(e1, e2); // 6
+    } else {
+      result.push(interval); // [[1,6], [8,10]]
+    }
+  }
+
+  return result;
+};
+//Time: SORT = n log n, LOOP = n
+// So n log n Time comp
+// Space: n
+
 // 05 13 2022
 /**
  * @param {number[][]} intervals
