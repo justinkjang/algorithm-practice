c9144a2c288beaf53ab629e4591152aa0d19736b
Justin Kyumin Jang <jangju99@gmail.com>
2022-06-27 18:25:04 -0700

    Recap 162 arrays

--- LeetCode/162. Find Peak Element.js
+++ LeetCode/162. Find Peak Element.js
@@ -1,3 +1,33 @@
+// 06 27 2022
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  // for log n complexity, we have to use binary search
+  // set left and right pointer
+  // while left < right, get mid number and compare it to mid + 1
+  // if mid is smaller move left pointer to potential peak
+  // if mid is bigger, move right pointer to potential peak
+
+  // Time: log n / Space: 1
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+};
+
 /**
  * @param {number[]} nums
  * @return {number}
