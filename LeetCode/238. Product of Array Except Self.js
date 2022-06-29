02c98ee6730c5a8df4bd13d002dc80ff96a98654
Justin Kyumin Jang <jangju99@gmail.com>
2022-06-28 18:23:37 -0700

    Explain 238 dynamic programming

--- LeetCode/238. Product of Array Except Self.js
+++ LeetCode/238. Product of Array Except Self.js
@@ -1,3 +1,33 @@
+// 06 28 2022
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  // dynamic programming
+  // create arrays, left shifted and right shifted
+  // for [1,2,3,4]
+  // left: [1, 1, 2, 6]
+  // righ: [24, 12, 4, 1]
+
+  // Time: linear
+
+  let left = new Array(nums.length).fill(1);
+  let right = new Array(nums.length).fill(1);
+
+  for (let i = 1; i < nums.length; i++) {
+    left[i] = left[i - 1] * nums[i - 1];
+  }
+  for (let i = nums.length - 2; i >= 0; i--) {
+    right[i] = right[i + 1] * nums[i + 1];
+  }
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = left[i] * right[i];
+  }
+
+  return nums;
+};
+
 /**
  * @param {number[]} nums
  * @return {number[]}
