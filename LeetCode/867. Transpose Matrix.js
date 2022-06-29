544f1e88abb11ca8b74223c88de3412f87250395
Justin Kyumin Jang <jangju99@gmail.com>
2022-06-29 14:45:08 -0700

    Recap 867 matrix array

--- LeetCode/867. Transpose Matrix.js
+++ LeetCode/867. Transpose Matrix.js
@@ -1,3 +1,27 @@
+// 06 29 2022
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function (matrix) {
+  // create copy of empty matrix
+  // switch [row][col] to result array's [col][row]
+  // Time: n^2, Space: n
+
+  // important to transpose the empty matrix
+  let result = Array.from({ length: matrix[0].length }, () =>
+    new Array(matrix.length).fill(0)
+  );
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      result[col][row] = matrix[row][col];
+    }
+  }
+
+  return result;
+};
+
 /**
  * @param {number[][]} matrix
  * @return {number[][]}
