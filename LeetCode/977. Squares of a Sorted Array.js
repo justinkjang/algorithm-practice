50124ea6470075c81fb0af4ef46ff3d7e2725b4b
Justin Kyumin Jang <jangju99@gmail.com>
2022-06-29 14:08:25 -0700

    Explain 977 two pointer array

--- LeetCode/977. Squares of a Sorted Array.js
+++ LeetCode/977. Squares of a Sorted Array.js
@@ -1,3 +1,44 @@
+// 06 29 2022
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  // each number is squared
+  // sort array increasing order
+  // Time: n log n, Space: constant
+
+  // more optimized way, we can use two pointer system
+  // only possible because nums array is already sorted
+  // create a copy of empty nums array
+  // set idx variable and left and right var
+  // compare squares of left and right, bigger number is places and that pointer is moved and decrease idx vairable.
+
+  // Time: linear, Space: linear
+
+  let result = new Array(nums.length).fill(0);
+  let idx = nums.length - 1;
+  let right = nums.length - 1;
+  let left = 0;
+
+  while (left <= right) {
+    let leftVal = Math.pow(nums[left], 2);
+    let rightVal = Math.pow(nums[right], 2);
+
+    if (leftVal > rightVal) {
+      result[idx] = leftVal;
+      left++;
+      idx--;
+    } else {
+      result[idx] = rightVal;
+      right--;
+      idx--;
+    }
+  }
+
+  return result;
+};
+
 /**
  * @param {number[]} nums
  * @return {number[]}
