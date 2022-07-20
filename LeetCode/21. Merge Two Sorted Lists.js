9acf0c22e559a03ad5841856e5e5672150f611a0
Justin Kyumin Jang <jangju99@gmail.com>
2022-07-19 23:08:49 -0400

    Solve 21 Linked List

--- LeetCode/21. Merge Two Sorted Lists.js
+++ LeetCode/21. Merge Two Sorted Lists.js
@@ -1,3 +1,46 @@
+// 07 19 2022
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  // create dummy node
+  // while l1 and l2 exist, compare and attach the dummy to smaller one
+  // move next of the smaller list to its next
+  // move node pointer to its next
+  // attach remaining nodes
+
+  // Time = n, Space = 1 (because we attaching linked list)
+
+  let head = new ListNode();
+  let node = head;
+
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      node.next = list2;
+      list2 = list2.next;
+    } else {
+      node.next = list1;
+      list1 = list1.next;
+    }
+
+    //moving node pointer to next
+    node = node.next;
+  }
+
+  node.next = list1 || list2;
+
+  return head.next;
+};
+
 // MY ANSWER O(3n)
 
 /**
