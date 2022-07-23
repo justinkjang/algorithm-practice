52e00be818c502608aa00a003aa749ee1fd7ba9d
Justin Kyumin Jang <jangju99@gmail.com>
2022-07-23 17:59:05 -0400

    Solve 242 anagram hash

--- LeetCode/242. Valid Anagram.js
+++ LeetCode/242. Valid Anagram.js
@@ -1,3 +1,33 @@
+// 07 23 2022
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  // use hashmap with
+  // key as char, val as num of char
+
+  const hash = {};
+  for (let char of s) {
+    if (!hash[char]) {
+      hash[char] = 1;
+    } else {
+      hash[char] += 1;
+    }
+  }
+
+  for (let char of t) {
+    if (!hash[char]) {
+      return false;
+    } else {
+      hash[char]--;
+    }
+  }
+
+  return Object.values(hash).every((val) => val === 0);
+};
+
 /**
  * @param {string} s
  * @param {string} t
