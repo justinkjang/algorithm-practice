73ba4c9b81a864323177bdf2ca7fe0d033cec922
Justin Kyumin Jang <jangju99@gmail.com>
2022-07-24 12:22:28 -0400

    Solve 235 binary search tree

---
+++ LeetCode/235. Lowest Common Ancestor of a Binary Search Tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  // BST Binary Search Tree have its nodes in In Order Traversal: Left, Top, Right
+  // Time = n, Space = 1
+
+  while (root) {
+    if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else {
+      break;
+    }
+  }
+
+  return root;
+};
