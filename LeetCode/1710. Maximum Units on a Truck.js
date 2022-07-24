18b640cc4c9d9f7ac96f0b61724185ae8d834ff4
Justin Kyumin Jang <jangju99@gmail.com>
2022-07-24 14:31:32 -0400

    Solve 1710 2d array

--- LeetCode/1710. Maximum Units on a Truck.js
+++ LeetCode/1710. Maximum Units on a Truck.js
@@ -1,3 +1,33 @@
+//07 24 2022
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+var maximumUnits = function (boxTypes, truckSize) {
+  // sort 2d by unit
+  // if size is greater or equal to target = return target * unit
+  // else update target, update unit, move to next
+  // Time = N log n, Space 1
+
+  let target = truckSize;
+  let max = 0;
+
+  boxTypes.sort((a, b) => b[1] - a[1]);
+
+  for (let box of boxTypes) {
+    if (box[0] >= target) {
+      max += target * box[1];
+      return max;
+    } else {
+      target -= box[0];
+      max += box[0] * box[1];
+    }
+  }
+
+  return max;
+};
+
 // 07 13 2022
 /**
  * @param {number[][]} boxTypes
