ed441ba0f3526eddf99794945d3674a0653c25a7
Justin Kyumin Jang <jangju99@gmail.com>
2022-07-28 15:22:12 -0400

    Solve 141 circular linked list

--- LeetCode/141. Linked List Cycle.js
+++ LeetCode/141. Linked List Cycle.js
@@ -1,3 +1,35 @@
+//07 28 2022
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  // circular linked list : fast and slow pointer
+  // Time: n, Space: 1
+
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 // 04 21 2022
 /**
  * Definition for singly-linked list.
